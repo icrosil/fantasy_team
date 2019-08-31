164d2ff878a1426095e4fb74eb23aafe9b531a5a
Illia Olenchenko <icrosil@gmail.com>
2019-08-31 20:20:23 +0300

    added simple express app

--- api/scraper/index.js
+++ api/scraper/index.js
@@ -1,1 +1,9 @@
-console.log('Scraper API');
+import express from 'express';
+import config from 'config';
+
+const app = express();
+const port = config.get('api.PORT');
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
